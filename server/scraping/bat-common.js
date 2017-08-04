32fbf22d98eb8b54d16bf2dbf676af67db02214c
yngittest <kanatto625@gmail.com>
2017-08-04 21:47:43 +0900

    refactor scraping batch entry

---
+++ server/scraping/bat-common.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function() {
+  // Set default node environment to development
+  var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+  if(env === 'development' || env === 'test') {
+    // Register the Babel require hook
+    require('babel-register');
+  }
+
+  const mongoose = require('mongoose');
+  mongoose.Promise = global.Promise;
+  const config = require('../config/environment');
+
+  // Connect to MongoDB
+  mongoose.connect(config.mongo.uri, config.mongo.options);
+  mongoose.connection.on('error', function(err) {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(-1); // eslint-disable-line no-process-exit
+  });  
+};
--- server/scraping/bat-get-comics.js
+++ server/scraping/bat-get-comics.js
@@ -1,23 +1,7 @@
 'use strict';
 
-// Set default node environment to development
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if(env === 'development' || env === 'test') {
-  // Register the Babel require hook
-  require('babel-register');
-}
-
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const config = require('../config/environment');
-
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error(`MongoDB connection error: ${err}`);
-  process.exit(-1); // eslint-disable-line no-process-exit
-});
+const batCommon = require('./bat-common');
+batCommon();
 
 const getComics = require('./get-comics');
 console.log('scraping start!');
